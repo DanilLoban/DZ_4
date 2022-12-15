20cb664b33ec87989e25fa1199e0017754017aea
Danil <dan.loban@yandex.ru>
2022-12-15 21:45:36 +0300

    firstly

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число, ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число, ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine(result);
